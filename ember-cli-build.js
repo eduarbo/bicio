eac111afc87a496391a1c7203e95157f1a2733bf
Eduardo Ruiz <eduarbo@gmail.com>
2015-10-18 18:47:34 +0000

    deploy

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+      fingerprint: {
+          exclude: [],
+          prepend: '/bicio/'
+      }
   });
   app.import('bower_components/mapbox.js/mapbox.js');
   app.import('bower_components/mapbox.js/mapbox.css');
