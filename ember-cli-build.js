233ad37a97a6096801a4d1a63a7a172da7f13ce9
Eduardo Ruiz <eduarbo@gmail.com>
2015-10-19 18:34:40 +0000

    disable fingerprint for dev

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
       fingerprint: {
-          enabled: true,
           exclude: [],
           prepend: 'http://eduarbo.github.io/bicio/'
       }
